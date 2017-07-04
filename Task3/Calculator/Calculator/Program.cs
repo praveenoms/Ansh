8b51fbb755e86a7300ff06a8cccc410c44c5d6d1
praveen <praveen@omscorps.com>
2017-07-04 18:54:51 +0530

    Task3

---
+++ Task3/Calculator/Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorLibrary;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations _MthOperation = new MathOperations();
+            if (args.Count() > 0)
+            {
+                switch (args[0].ToString().ToUpper())
+                {
+                    case "SUM":
+                       _MthOperation.Sum((args.Count() > 1 ? args[1].ToString() : "0")); ; // Check if any Argument is passed after the operation
+                        break;
+                    case "ADD":
+                        _MthOperation.Add((args.Count() > 1 ? args[1].ToString() : "0")); // Check if any Argument is passed after the operation
+                        break;
+
+                }
+            }
+        }
+    }
+}
