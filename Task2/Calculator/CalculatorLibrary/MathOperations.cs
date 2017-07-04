2a30b12ddee6cb4583435fde43c06937067afbce
praveen <praveen@omscorps.com>
2017-07-04 18:40:49 +0530

    Task2 commit

---
+++ Task2/Calculator/CalculatorLibrary/MathOperations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class MathOperations
+    {
+
+        /// <summary>
+        /// Sum Function -- Add up the numerical values seperated by ',' in second argument
+        /// </summary>
+        /// <param name="Args"></param>
+        public int Sum(string Args)
+        {
+            try
+            {
+                int sum = 0;
+                
+                var Arguments=Args.Split(',');
+                if (Arguments.Count() == 2)
+                {
+                    Console.WriteLine(" The Sum of Given Numbers is  {0}", (Convert.ToInt16(Arguments[0]) + Convert.ToInt16(Arguments[1])));
+                    return (Convert.ToInt16(Arguments[0]) + Convert.ToInt16(Arguments[1]));
+                }
+                else if (Arguments.Count() == 1)
+                {
+                    Console.WriteLine(" The Sum of Given Numbers is  {0}", Arguments[0]);
+                    return Convert.ToInt16(Arguments[0]);
+                }
+                else
+                return sum;
+            }
+            catch { throw new ArgumentException(); }
+        }
+
+        /// <summary>
+        /// Sum Function -- Add up the numerical values seperated by ',' in second argument
+        /// </summary>
+        /// <param name="Args"></param>
+        public int Add(string Args)
+        {
+            try
+            {
+                int sum = 0;
+                foreach (string val in Args.Split(','))
+                {
+                    sum += Convert.ToInt16(val);
+                }
+                Console.WriteLine(" The Sum of Given Numbers is  " + sum);
+                return sum;
+            }
+            catch { throw new ArgumentException(); }
+        }
+    }
+}
---
+++ Task2/Calculator/CalculatorLibraryTest/MathOperationsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorLibrary;
+namespace CalculatorLibraryTest
+{
+    [TestClass]
+    class MathOperationsTest
+    {
+        [TestMethod]
+        public void sumTest()
+        {
+            var _Mo = new MathOperations();
+
+            int expected = 5;
+            int actual = _Mo.Sum("2,3");
+            Assert.AreEqual(expected, actual, "The Expected result is not equal to Actual result");
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var _Mo = new MathOperations();
+
+            int expected = 133;
+            int actual = _Mo.Sum(" 4,7,3,4,7,3,5,6,7,4,3,2,5,7,5,3,4,6,7,8,9,5,5,5,4,3,2");
+            Assert.AreEqual(expected, actual, "The Expected result is not equal to Actual result");
+        }
+    }
+}
