92987c4384cc9260e061a978e7cbac13b8e5e036
praveen <praveen@omscorps.com>
2017-07-04 18:21:16 +0530

    Task 1 Commit

---
+++ Task1/Calculator/CalculatorLibraryTest/MathOperationsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorLibrary;
+namespace CalculatorLibraryTest
+{
+    [TestClass]
+    class MathOperationsTest
+    {
+        [TestMethod]
+        public void sumTest()
+        {
+            var _Mo = new MathOperations();
+
+            int expected = 5;
+            int actual = _Mo.Sum("2,3");
+            Assert.AreEqual(expected, actual, "The Expected result is not equal to Actual result");
+        }
+    }
+}
